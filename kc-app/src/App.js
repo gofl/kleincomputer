55a52cefef58f6a2a7b0ba9602f54a93ba768a77
gofl <christof.heymann@gmail.com>
2021-11-23 00:22:02 +0100

    Create terminal logo

--- kc-app/src/App.js
+++ kc-app/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Kleincomputer</h1>
+      <div className="logo-header-content">
+        <h1 className="logo-header-first text-9xl font-extrabold">KLEINCOMPUTER</h1>
+        <h1 className="logo-header-second text-9xl font-extrabold text-red-300">KLEINCOMPUTER</h1>
+        <h1 className="logo-header-third text-9xl font-extrabold text-blue-300">KLEINCOMPUTER</h1>
+      </div>
     </div>
   );
 }
